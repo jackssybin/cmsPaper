dab6330ac1634228cb73be6c710f5b83e18e51e0
27208461@qq.com <ruguojay258>
2016-07-05 15:03:12 +0800

    添加了一个常量

--- cms_paper/src/com/yiyun/util/Constants.java
+++ cms_paper/src/com/yiyun/util/Constants.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Constants {
-	/******************************数据接口常量开始********************************************/
+	/******************************鏁版嵁鎺ュ彛甯搁噺寮�濮�********************************************/
 	
 	public final static String MOBILE_PARAM_SHOW_COUNT_KEY = "showCount";
 	public final static String MOBILE_PARAM_CURRENT_PAGE_KEY = "currentPage";
@@ -15,43 +15,44 @@ public class Constants {
 	public final static String MOBILE_PARAM_RESP_CODE_ERROR = "ERROR";
 	public final static String MOBILE_PARAM_RESP_MSG_KEY = "respMsg";
 	public final static String MOBILE_PARAM_RESP_MSG_SUCCESS = "SUCCESS";
-	/******************************数据接口常量结束********************************************/
-	public static String PICTURE_VISIT_FILE_PATH = "";//图片访问的路径
+	public final static String MOBILE_PARAM_CHANNEL_ID_KEY = "channelId";
+	/******************************鏁版嵁鎺ュ彛甯搁噺缁撴潫********************************************/
+	public static String PICTURE_VISIT_FILE_PATH = "";//鍥剧墖璁块棶鐨勮矾寰�
 	
-	public static String PICTURE_SAVE_FILE_PATH = "";//图片存放的路径
+	public static String PICTURE_SAVE_FILE_PATH = "";//鍥剧墖瀛樻斁鐨勮矾寰�
 	
 	public static String SUCCESS = "SUCCESS";
 	
 	public static String FAIL = "FAIL";
 	
-	//微信预订单创建-成功状态
+	//寰俊棰勮鍗曞垱寤�-鎴愬姛鐘舵��
 	public static String PREODER_CREATE_SUCCESS = "0";
 	
-	//微信订单支付-成功状态
+	//寰俊璁㈠崟鏀粯-鎴愬姛鐘舵��
 	public static String ORDER_PAY_SUCCESS = "1";
 	
-	//微信订单支付-支付失败(其他原因，如银行返回失败)状态
+	//寰俊璁㈠崟鏀粯-鏀粯澶辫触(鍏朵粬鍘熷洜锛屽閾惰杩斿洖澶辫触)鐘舵��
 	public static String ORDER_PAY_PAYERROR = "2";
 	
-	//微信订单支付-转入退款状态
+	//寰俊璁㈠崟鏀粯-杞叆閫�娆剧姸鎬�
 	public static String ORDER_PAY_REFUND = "3";
 	
-	//微信订单支付—未支付状态
+	//寰俊璁㈠崟鏀粯鈥旀湭鏀粯鐘舵��
 	public static String ORDER_PAY_NOTPAY = "4";
 	
-	//微信订单支付—已关闭状态
+	//寰俊璁㈠崟鏀粯鈥斿凡鍏抽棴鐘舵��
 	public static String ORDER_PAY_CLOSED = "5";
 	
-	//微信订单支付—已撤销状态
+	//寰俊璁㈠崟鏀粯鈥斿凡鎾ら攢鐘舵��
 	public static String ORDER_PAY_REVOKED = "6";
 	
-	//微信订单支付-用户支付中状态
+	//寰俊璁㈠崟鏀粯-鐢ㄦ埛鏀粯涓姸鎬�
 	public static String ORDER_PAY_USERPAYING = "7";
 	
-	//微信订单支付-未支付(输入密码或确认支付超时)状态
+	//寰俊璁㈠崟鏀粯-鏈敮浠�(杈撳叆瀵嗙爜鎴栫‘璁ゆ敮浠樿秴鏃�)鐘舵��
 	public static String ORDER_PAY_NOPAY = "8";
 	
-	//微信订单查询接口中“trade_state”参数与本系统状态关系
+	//寰俊璁㈠崟鏌ヨ鎺ュ彛涓�渢rade_state鈥濆弬鏁颁笌鏈郴缁熺姸鎬佸叧绯�
 	public static Map<String , String> ORADER_STATUS_MAP = new HashMap<String , String>(){
 		private static final long serialVersionUID = -223374791341026460L;
 		{
@@ -66,63 +67,63 @@ public class Constants {
 		}
 	};
 	
-	//默认role_id
+	//榛樿role_id
 	public static String DEFAULT_ROLE_ID="2";//TODO
 	public static String DEFAULT_PWD="123456";//TODO
 	
-	//微信证书路径
+	//寰俊璇佷功璺緞
 	public static String CREDENTIAL_PATH = "/root/wxpay/cert/apiclient_cert.p12";//TODO
 //	public static String CREDENTIAL_PATH = "D:\\resourcesfile\\wxpay\\cert\\apiclient_cert.p12";//TODO
 	
 	
-	//本机IP
+	//鏈満IP
 	public static String LOCAL_IP = "127.0.0.1";//TODO
 	
 	
-	//微信支付分配的商户号
+	//寰俊鏀粯鍒嗛厤鐨勫晢鎴峰彿
 	public static String MCHID = "1335782401";//TODO
 	
-	//商户支付密钥Key
+	//鍟嗘埛鏀粯瀵嗛挜Key
 	public static String KEY = "eyeglass258EYEGLASS258jackssybin";//TODO
 	
-	//微信分配的公众账号ID--适目以戴
-	public static String APPID = "wx462d349421581949";//正确
-//	public static String APPID = "wxb821cf72430cc89b";//测试
+	//寰俊鍒嗛厤鐨勫叕浼楄处鍙稩D--閫傜洰浠ユ埓
+	public static String APPID = "wx462d349421581949";//姝ｇ‘
+//	public static String APPID = "wxb821cf72430cc89b";//娴嬭瘯
 	
-	//公众号的appsecret--适目以戴
-//	public static String SECRET = "f86038291998d39380ca56573f7da7c5";//测试
-	public static String SECRET = "e387ce551838bc7232bd9e21858daa34";//正确
+	//鍏紬鍙风殑appsecret--閫傜洰浠ユ埓
+//	public static String SECRET = "f86038291998d39380ca56573f7da7c5";//娴嬭瘯
+	public static String SECRET = "e387ce551838bc7232bd9e21858daa34";//姝ｇ‘
 	
-	//消息加密密钥
+	//娑堟伅鍔犲瘑瀵嗛挜
 	public static String ENCODINGAESKEY = "oU0nPKZd2wGXDIleqOaB8vLnVMbGzHIbdtAW2IXbwWx";
 	
-	//公众号的token
+	//鍏紬鍙风殑token
 	public static String TOKEN = "dfgertery4578vcbt76afbfu67";//TODO
 
-	//通知地址，接收微信支付成功通知
+	//閫氱煡鍦板潃锛屾帴鏀跺井淇℃敮浠樻垚鍔熼�氱煡
 	public static String NOTIFY_URL = "http://www.yiku-yiku.com/eyeTest/weixin/wxpaynotify";//TODO
 
-	//授权后重定向的回调链接地址
+	//鎺堟潈鍚庨噸瀹氬悜鐨勫洖璋冮摼鎺ュ湴鍧�
 	public static String REDIRECT_URI = "http://www.yiku-yiku.com/eyeTest/weixin/autoLogin";//TODO
 //	public static String REDIRECT_URI = "http://ecloudtime.com/eyeTest/weixin/autoLogin";//TODO
-	//废弃
+	//搴熷純
 //	public static String SCOPE_BASE = "snsapi_base";
-	//废弃
+	//搴熷純
 //	public static String SCOPE_USERINFO = "snsapi_userinfo";
-	//废弃
-	//微信获取code url,用code换openid
+	//搴熷純
+	//寰俊鑾峰彇code url,鐢╟ode鎹penid
 //	public static String  CODE_URL = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE&state=STATE#wechat_redirect"; 
 
-	//微信获取openid url
+	//寰俊鑾峰彇openid url
 	public static String  OPENID_URL = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
 	
-	//微信获取用户信息url
+	//寰俊鑾峰彇鐢ㄦ埛淇℃伅url
 	public static String  USERINFO_URL = "https://api.weixin.qq.com/sns/userinfo?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN";
 	
-	//微信统一支付接口url
-	public static String UNIFIEDORDER_URL = "https://api.mch.weixin.qq.com/pay/unifiedorder";//不能修改
+	//寰俊缁熶竴鏀粯鎺ュ彛url
+	public static String UNIFIEDORDER_URL = "https://api.mch.weixin.qq.com/pay/unifiedorder";//涓嶈兘淇敼
 	
-	//微信订单查询接口url
+	//寰俊璁㈠崟鏌ヨ鎺ュ彛url
 	public static String ORDERQUERY_URL = "https://api.mch.weixin.qq.com/pay/orderquery";//TODO
 	
 	public static String getPICTURE_VISIT_FILE_PATH() {
